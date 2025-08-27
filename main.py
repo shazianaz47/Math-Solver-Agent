de5d3fe82d036695cd4b13d297c5a1d13ecba3e1
shazia naz <shazianaz2417@gmail.com>
2025-08-28 00:13:45 +0500

    first commit

--- main.py
+++ main.py
@@ -1,28 +1,49 @@
 import os
-from agents import Agent,OpenAIChatCompletionsModel, Runner
-from openai import AsyncOpenAI
+from agents import Agent,OpenAIChatCompletionsModel,AsyncOpenAI, RunConfig, Runner
 from dotenv import load_dotenv
 
 #Load environment variables from .env file
 load_dotenv()
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
-BASE_URL= "https://openrouter.ai/api/v1"
+
+
+gemini_api_key = os.getenv("OPENROUTER_API_KEY")
+# print("Gemini API Key:", gemini_api_key)    
+
+# Check if the API key is set
+if not gemini_api_key:
+    raise ValueError("OPENROUTER_API_KEY is not set in the environment variables.") 
+
 
 # Initialize the OpenAI client with your API key and base URL
-client = AsyncOpenAI(
-    api_key=OPENROUTER_API_KEY,
-    base_url=BASE_URL,
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://openrouter.ai/api/v1"  # OpenRouter API base URL  
 )
 
-# Define the agent with the OpenAI model
+# Model Setup
+model=OpenAIChatCompletionsModel(   
+    model="deepseek/deepseek-r1-0528:free",#replace model if needed
+    openai_client=external_client,
+)
+
+# Setup Config
+config=RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True   
+)
+
+# Agent Setup
 agent = Agent(
     name="MathSolver",
-    instructions="Solve any math problem step-by-step.",
-    model=OpenAIChatCompletionsModel(   
-        openai_client=client,
-        model="deepseek/deepseek-r1-0528-qwen3-8b:free",
-    )
-)
+    instructions="You are helpful MathSolver.Solve any maths' problems step-by-step.",
+) 
 
-result = Runner.run_sync(agent,"What is the sqrt 64?")
+# Run the agent with a math problem input
+result = Runner.run_sync(
+    agent, 
+    input="What is 44*4?",
+    run_config=config,
+)
+# Print the final output from the agent
 print(result.final_output)
